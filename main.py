91b520f6b02a603be4d04d992a5439ec081d9aa9
Zeinab <84691251+Zeinab-Amini@users.noreply.github.com>
2021-12-31 06:52:24 +0000

    watched the video then modified most.

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ from replit import clear
 from art import logo
 print(logo)
 
-any_others = True
+name_and_bid = {}
+#not true but False!!!!
+any_others = False
+
+#did not even start on this part
+def hightest_bidder(bidding_record):
+    #trying to look at each one and see which one is the highest
+    for bidder in bidding_record:
+        high = 0
+        winner = ""
+        bid_amount = bidding_record[bidder]
+        if bid_amount > high:
+            bid_amount = high
+            winner = bidder
+    print(f"The winner is {winner} with the bid of ${high}")
+        
 
 while any_others == True:
+    #correct with the while loop
     name = input("Type your name here: ")
-    bid = input("Type your bid here: ")
-
-    name_and_bid = {
-        name : bid
-    }
-    input("Are there any other biders? 'yes' or 'no': ")
-    if any_others == 'no':
-        any_others = False
-        print("Wait")
-    elif any_others == 'yes':
+    bid = int(input("Type your bid here: $"))
+    #wrongly created dictionary
+    name_and_bid[name] = bid
+    #forgot to save it into a varible
+    should_conitue = input("Are there any other biders? 'yes' or 'no': ")
+    if should_conitue == 'no':
         any_others = True
+        hightest_bidder(bid)
+    elif should_conitue == 'yes':
         clear()
     
